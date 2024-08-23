eb71663ed7751b41faeb7e4aa6588f3d90d380ac
__Mohamed-Zayed__ <143558508+zayed4516@users.noreply.github.com>
2024-08-23 18:52:23 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,7 @@ st.title("🎈 car co2 emssion")
 f1=st.number_input('feature 1',min_value=1,max_value=10)
 f2=st.number_input('feature 2',min_value=1,max_value=10)
 f3=st.number_input('feature 3',min_value=1,max_value=10)
+
+with open('model.pkl','rd') as file:
+ model= pickle.load(file)
+model.predict() 
