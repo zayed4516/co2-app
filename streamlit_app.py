8bda22a9042d12180244de9ec916cc050226859a
__Mohamed-Zayed__ <143558508+zayed4516@users.noreply.github.com>
2024-08-23 18:49:21 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
-
+import pickle
 st.title("🎈 car co2 emssion")
-st.number_input('feature 1',min_value=1,max_value=10)
-st.number_input('feature 2',min_value=1,max_value=10)
-st.number_input('feature 3',min_value=1,max_value=10)
+f1=st.number_input('feature 1',min_value=1,max_value=10)
+f2=st.number_input('feature 2',min_value=1,max_value=10)
+f3=st.number_input('feature 3',min_value=1,max_value=10)
