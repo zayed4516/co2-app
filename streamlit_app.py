6b633be507d4ffecf27beeffc13cda90d0fcaca6
__Mohamed-Zayed__ <143558508+zayed4516@users.noreply.github.com>
2024-08-23 18:56:50 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,6 @@ f1=st.number_input('feature 1',min_value=1,max_value=10)
 f2=st.number_input('feature 2',min_value=1,max_value=10)
 f3=st.number_input('feature 3',min_value=1,max_value=10)
 
-with open('model.pkl','rd') as file:
+with open('model.pkl','rb') as file:
  model= pickle.load(file)
  
