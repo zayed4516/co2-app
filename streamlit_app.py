e6c4a345df4beb4eb877d9eb0638a54268190e86
__Mohamed-Zayed__ <143558508+zayed4516@users.noreply.github.com>
2024-08-23 18:47:42 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,5 @@ import streamlit as st
 
 st.title("🎈 car co2 emssion")
 st.number_input('feature 1',min_value=1,max_value=10)
+st.number_input('feature 2',min_value=1,max_value=10)
+st.number_input('feature 3',min_value=1,max_value=10)
